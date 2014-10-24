be27c1b3bef7fb12261306c7644129cdd5e75bc7
John Hughes <john@castlegateit.co.uk>
2014-10-24 14:02:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Castlegate IT WP CMB Gallery #
+
+Simple gallery field using [Custom Meta Boxes](https://github.com/castlegateit/cgit-wp-custom-meta-boxes). Defines a field called `gallery`, which can be displayed with the `get_field()`, `the_field()`, or `get_post_meta()` functions:
+
+    get_field('gallery', $post_id, FALSE);
+
+The third argument must be `FALSE` to return an array of gallery items. If it is not set, or it is set to `TRUE`, only the first item will be returned. The plugin also provides a shortcode (named `cgit_gallery` to avoid conflicting with the default WordPress gallery shortcode):
+
+    [cgit_gallery]
+
+This generates a gallery as a simple unordered list:
+
+    <div class="cgit-gallery">
+        <ul>
+            <li><a href="image.jpg" title="Example"><img src="thumb.jpg" alt="Example" /></a></li>
+            <li><a href="image.jpg" title="Example"><img src="thumb.jpg" alt="Example" /></a></li>
+        </ul>
+    </div>
+
+The default gallery fields can be edited with the `cgit_gallery_fields` filter.
---
+++ cgit-wp-cmb-gallery.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+
+Plugin Name: Castlegate IT WP CMB Gallery
+Plugin URI: http://github.com/castlegateit/cgit-wp-cmb-seo
+Description: Simple gallery field using Custom Meta Boxes.
+Version: 1.0
+Author: Castlegate IT
+Author URI: http://www.castlegateit.co.uk/
+License: MIT
+
+*/
+
+/**
+ * Add gallery fields
+ */
+function cgit_gallery_fields ($meta_boxes) {
+
+    $subfields = array(
+        array(
+            'id'   => 'image',
+            'name' => 'Image',
+            'type' => 'image',
+        ),
+        array(
+            'id'   => 'title',
+            'name' => 'Title',
+            'type' => 'text',
+        ),
+        array(
+            'id'   => 'alt',
+            'name' => 'Alternative text',
+            'type' => 'text',
+        ),
+    );
+
+    $fields = array(
+        array(
+            'id'         => 'gallery',
+            'name'       => 'Gallery',
+            'type'       => 'group',
+            'fields'     => $subfields,
+            'repeatable' => TRUE,
+        ),
+    );
+
+    $meta_box = array(
+        'id'      => 'cgit-wp-cmb-gallery',
+        'title'   => 'Gallery',
+        'pages'   => array('post', 'page'),
+        'context' => 'normal',
+        'fields'  => $fields,
+    );
+
+    $meta_boxes[] = apply_filters('cgit_gallery_fields', $meta_box);
+
+    return $meta_boxes;
+
+}
+
+add_filter('cmb_meta_boxes', 'cgit_gallery_fields');
+
+/**
+ * Add basic shortcode
+ */
+function cgit_gallery_shortcode () {
+
+    if ( ! class_exists('CMB_Meta_Box') ) {
+        return FALSE;
+    }
+
+    $gallery = get_field('gallery', FALSE, FALSE);
+    $output  = '';
+
+    if ($gallery) {
+
+        $output .= '<div class="cgit-gallery"><ul>';
+
+        foreach ($gallery as $item) {
+
+            $image = wp_get_attachment_image_src($item['image'], 'full');
+            $thumb = wp_get_attachment_image_src($item['image']);
+            $title = $item['title'];
+            $alt   = $item['alt'];
+
+            $output .= "<li><a href='{$image[0]}' title='$title'><img src='{$thumb[0]}' alt='$alt' /></a></li>";
+
+        }
+
+        $output .= '</ul></div>';
+
+    }
+
+    return $output;
+
+}
+
+add_shortcode('cgit_gallery', 'cgit_gallery_shortcode');
